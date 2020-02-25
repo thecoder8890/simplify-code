a47fac1cc10ac61f0a9d89b29e116d899e02740a
thecoder8890 <33773206+thecoder8890@users.noreply.github.com>
2020-02-25 13:43:16 +0530

    delete a node from linked list

---
+++ LinkedlistDeleteNode.java
@@ -0,0 +1,71 @@
+
+public class LinkedlistDeleteNode {
+
+	Node head; // head of list
+
+	/* Linked list Node */
+	class Node {
+		int data;
+		Node next;
+
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+	}
+
+	void deleteNode(int key) {
+		Node temp = head, prev = null;
+
+		if (temp != null && temp.data == key) {
+			head = temp.next;
+			return;
+
+		}
+		while (temp != null && temp.data != key) {
+			prev = temp;
+			temp = temp.next;
+
+		}
+		if(temp==null) return;
+		
+		prev.next=temp.next;
+
+	}
+	
+	public void push(int new_data) {
+		
+		Node new_node = new Node(new_data);
+		new_node.next=head;
+		head=new_node;
+		
+	}
+	  public void printList() 
+	    { 
+	        Node tnode = head; 
+	        while (tnode != null) 
+	        { 
+	            System.out.print(tnode.data+" "); 
+	            tnode = tnode.next; 
+	        } 
+	    } 
+	  
+	   public static void main(String[] args) 
+	    { 
+	        LinkedlistDeleteNode llist = new LinkedlistDeleteNode(); 
+	  
+	        llist.push(7); 
+	        llist.push(1); 
+	        llist.push(3); 
+	        llist.push(2); 
+	  
+	        System.out.println("\nCreated Linked list is:"); 
+	        llist.printList(); 
+	  
+	        llist.deleteNode(7); // Delete node at position 4 
+	  
+	        System.out.println("\nLinked List after Deletion at position 4:"); 
+	        llist.printList(); 
+	    } 
+
+}
